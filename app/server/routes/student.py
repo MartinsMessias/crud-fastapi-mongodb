f852471ca5928a3a00d64ec05f91ed404489f94e
MartinsMessias <messiasads2017@gmail.com>
2020-09-24 22:14:14 -0500

    Rotas para recuperar todos os alunos e um único aluno

--- app/server/routes/student.py
+++ app/server/routes/student.py
@@ -3,7 +3,7 @@
 
 """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 
 from app.server.database import(
@@ -29,3 +29,19 @@ async def add_student_data(student: StudentSchema = Body(...)):
     student = jsonable_encoder(student)
     new_student = await add_student(student)
     return ResponseModel(new_student, 'Student added successfully.')
+
+
+@router.get('/', response_description="Students retrieved")
+async def get_students():
+    students = await retrieve_students()
+    if students:
+        return ResponseModel(students, 'Students data retrieved successfully')
+    return ResponseModel(students, 'Empty list returned')
+
+
+@router.get('/{id}', response_description="Student data retrieved")
+async def get_student_data(id):
+    student = await retrieve_student(id)
+    if student:
+        return ResponseModel(student, 'Student data retrieved successfully')
+    return ErrorResponseModel('An error occurred', 404, 'Student doesn\'t exist.')
