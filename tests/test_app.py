752765ad4c096cc1114246bd74e3620dec23f1e3
MartinsMessias <messiasads2017@gmail.com>
2020-10-07 18:47:22 -0500

    Teste em listar estudantes

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,8 +9,8 @@ def test_read_main():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to this fantastic app!"}
-#
-# def test_list_students():
-#     response = client.get('/student/')
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "Welcome to this fantastic app!"}
+
+def test_read_students_list():
+    response = client.get('/student/')
+    assert response.status_code == 200
+    assert response.json() == {"message": "Students data retrieved successfully"}
